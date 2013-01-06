a8c2c6e81b436970e1d377fa0fd43f4baf09e2e8
lbzhao28 <lbzhao28@gmail.com>
2013-01-06 11:51:19 +0800

    根据下拉框的文本选择下拉框,用在多级联动里.

--- static/js/localGrpUsrList.js
+++ static/js/localGrpUsrList.js
@@ -10,15 +10,15 @@ function createGrpUsrList()
     //TODO: from xml load data.
     var arrayGrpUsr=new Array();
     arrayGrpUsr[0]=new Array(1,1,0,"");
-    arrayGrpUsr[1]=new Array("ccbgroup1",1,0,"ccbgroup1");
-    arrayGrpUsr[2]=new Array("ccbgroup2",1,0,"ccbgroup2");
-    arrayGrpUsr[3]=new Array(4,2,"ccbgroup1","");
-    arrayGrpUsr[4]=new Array("11018",2,"ccbgroup1","11018");
-    arrayGrpUsr[5]=new Array("11001",2,"ccbgroup1","11001");
-    arrayGrpUsr[6]=new Array("11002",2,"ccbgroup1","11002");
-    arrayGrpUsr[7]=new Array(8,2,"ccbgroup2","");
-    arrayGrpUsr[8]=new Array("41001",2,"ccbgroup2","41001");
-    arrayGrpUsr[9]=new Array("41002",2,"ccbgroup2","41002");
+    arrayGrpUsr[1]=new Array(2,1,0,"ccbgroup1");
+    arrayGrpUsr[2]=new Array(3,1,0,"ccbgroup2");
+    arrayGrpUsr[3]=new Array(4,2,2,"");
+    arrayGrpUsr[4]=new Array(5,2,2,"11018");
+    arrayGrpUsr[5]=new Array(6,2,2,"11001");
+    arrayGrpUsr[6]=new Array(7,2,2,"11002");
+    arrayGrpUsr[7]=new Array(8,2,3,"");
+    arrayGrpUsr[8]=new Array(9,2,3,"41001");
+    arrayGrpUsr[9]=new Array(10,2,3,"41002");
 
     return arrayGrpUsr;
 }
